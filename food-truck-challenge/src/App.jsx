ed6484a9ba1440094c50d941eb96fea855fe15c5
Virginia Wilcox <wilcoxva@gmail.com>
2023-03-08 16:00:26 -0500

    gave app a background color and header text

---
+++ food-truck-challenge/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ background: 'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(255,255,255,1) 100%)', height: '100vh' }}>
+      <header style={{ textAlign: 'center', marginTop: '100px' }}>
+        <h1 style={{ fontFamily: 'helvetica' }}>
+          Your marketing destination for information on all of San Francisco's various food trucks.
+        </h1>
+      </header>
+      <body />
+    </div>
+   
+  );
+}
+
+export default App;
